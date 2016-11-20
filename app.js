5607784f8298959e41028ab34fbbadfa50d67963
redhendery <redhendery@gmail.com>
2016-11-20 11:13:03 -0800

    change how dev port is set

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'));
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 app.get('/', (req, res) => {
   res.render('index', { title: 'Countdown Timer' })
 })
 
-app.listen(5000, () => {
-  console.log('App is hosted at http://localhost:5000')
-})
+// app.listen(5000, () => {
+//   console.log('App is hosted at http://localhost:5000')
+// })
 
 module.exports = app;
